0fd1a1ed47c98e215a1b4cd40e6c0f133d08a021
Norbert Janik <norbert.janik@protonmail.com>
2026-06-11 10:24:08 +0200

    Updated monitors profiles layout for easier script editing.

--- config/monitor_profiles.lua
+++ config/monitor_profiles.lua
@@ -1,7 +1,6 @@
 local profiles = {
     laptop = {
         primary = "InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x057D",
-
         monitors = {
             {
                 desc = "InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x057D",
@@ -14,7 +13,6 @@ local profiles = {
 
     dock = {
         primary = "Lenovo Group Limited P40w-20 V909507G",
-
         monitors = {
             {
                 desc = "ASUSTek COMPUTER INC ASUS MB14AHD S6LMTF011157",
@@ -22,7 +20,6 @@ local profiles = {
                 position = "2800x1350",
                 scale = 1.2,
             },
-
             {
                 desc = "Lenovo Group Limited P40w-20 V909507G",
                 mode = "5120x2160@75",
@@ -34,7 +31,6 @@ local profiles = {
 
     triple = {
         primary = "Lenovo Group Limited P40w-20 V909507G",
-
         monitors = {
             {
                 desc = "Lenovo Group Limited P40w-20 V909507G",
@@ -42,20 +38,18 @@ local profiles = {
                 position = "0x0",
                 scale = 1.6,
             },
-
             {
                 desc = "ASUSTek COMPUTER INC ASUS MB14AHD S6LMTF011157",
                 mode = "1920x1080@60.01000",
                 position = "2800x1350",
                 scale = 1.2,
             },
-
             {
                 desc = "InfoVision Optoelectronics (Kunshan) Co.Ltd China 0x057D",
                 disabled = true,
-            }
+            },
         }
-    }
+    },
 }
 
 return profiles
