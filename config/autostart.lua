1e424ae1e3b917071c686c0a3f6e12c9449690cc
Norbert Janik <norbert.janik@protonmail.com>
2026-06-19 11:01:48 +0200

    Updated swww to awww. Added audio restart for clean graph for steam.

--- config/autostart.lua
+++ config/autostart.lua
@@ -9,7 +9,7 @@ on_start("hyprland.start", function()
     hl.exec_cmd("udiskie")
     hl.exec_cmd("lxqt-policykit-agent")
 
-    hl.exec_cmd("swww-daemon")
+    hl.exec_cmd("awww-daemon")
     hl.exec_cmd("xembedsniproxy")
 
     hl.exec_cmd("wayscriber --daemon")
@@ -22,5 +22,6 @@ on_start("hyprland.start", function()
 
     hl.exec_cmd("bash ~/.config/quickshell/scripts/light-dark.sh wallpaper")
     hl.exec_cmd("bash ~/.config/hypr/scripts/apply-input-mapping.sh")
+    hl.exec_cmd("systemctl --user restart pipewire pipewire-pulse wireplumber")
 
 end)
